be4222ee4b499bc1a4000f25b497b109b58ceb8e
Will Oliver <willoliver.main@gmail.com>
2025-05-15 22:19:41 +0100

    wip: notes and signature of routine for "async-ifying" the main programme routine

--- include/sched/spindle.h
+++ include/sched/spindle.h
@@ -5,6 +5,7 @@
 #include "job.h"
 #include "ds/ring.h"
 #include "ds/dl_list.h"
+#include <pthread.h>
 
 struct __attribute__((packed)) spindle {
 	fiber_t *fibers;
@@ -20,5 +21,17 @@ void spindle_del(struct spindle *s);
 int spindle_do(struct spindle *s, struct job j);
 
 void spindle_loop(struct spindle *s);
+
+/* assimilates the current stack and execution context, */
+/* returning a spindle which owns the current process */
+/* as an asynchronous job */
+struct spindle *spindle_hijack();
+
+/* notes to self - as our hijack routine returns the spindle, */
+/* we can place it at the top of the stack of main() or any async-able */
+/* routine before anything else is placed on the stack.  */
+/**/
+/* that allows us to pinch the return address and stack without  */
+/* preventing library users from adding to the stack their own data. */
 
 #endif
